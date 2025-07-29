3a6b1a08e5b0f324869d9ae623e8b43e9e47f265
kaebalsaebal <ssassaium@gmail.com>
2025-07-29 15:02:32 +0900

    test commit

---
+++ src/main/java/com/ddiring/backend_asset/BackendAssetApplication.java
@@ -0,0 +1,14 @@
+package com.ddiring.backend_asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BackendAssetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddiring/backend_asset/BackendAssetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiring.backend_asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
