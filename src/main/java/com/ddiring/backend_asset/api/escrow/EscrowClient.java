ea46eb70275b23dd5eabbe461c2f9c6754e40994
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-27 13:40:58 +0900

    에셋 으로보내기

--- src/main/java/com/ddiring/backend_asset/api/escrow/EscrowClient.java
+++ src/main/java/com/ddiring/backend_asset/api/escrow/EscrowClient.java
@@ -2,12 +2,13 @@ package com.ddiring.backend_asset.api.escrow;
 
 import com.ddiring.backend_asset.common.dto.ApiResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "escrowClient", url = "${escrow.base-url}")
 public interface EscrowClient {
     @PostMapping("/api/escrow/deposit")
-    ApiResponseDto<EscrowDto> escrowDeposit(@RequestBody EscrowDto escrowDto);
+    ResponseEntity<String> escrowDeposit(@RequestBody EscrowDto escrowDto);
 
 }
